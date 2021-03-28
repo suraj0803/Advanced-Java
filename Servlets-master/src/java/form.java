7e895ddf0ce49e4a6d7792aeca2c9f0141e4c225
Kuldeep Dwivedi <64473358+suraj0803@users.noreply.github.com>
2021-03-28 09:50:06 +0530

    Add files via upload

---
+++ Servlets-master/src/java/form.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class form extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html");
+ 
+    PrintWriter out = response.getWriter();
+        out.print("<html><body background=\"image.jpeg\" style=\" background-repeat:no-repeat;background-size:100%\">"
+                +"</html></body");
+   
+   
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+   
+   
+        String p = request.getParameter("Username");
+        String n = request.getParameter("Password");
+        
+        if(p.equals("Kuldeep")&n.equals("qwerty")){
+        RequestDispatcher rd=request.getRequestDispatcher("Welcome");  
+        rd.forward(request, response);  
+    }  
+    else{  
+        out.print("Sorry UserName or Password Error!"); 
+        out.print("  not a valid user");
+        RequestDispatcher rd=request.getRequestDispatcher("/form.html");  
+        rd.include(request, response);  
+                      
+        }  
+    }  
+  
+}  
